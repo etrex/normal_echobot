5404e47e250d85b9cc7072ffe02b6c819d506966
etrex kuo <et284vu065k3@gmail.com>
2019-01-04 14:10:16 +0800

    init

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,24 @@
+require 'line/bot'
+
+class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    body = request.body.read
+    events = client.parse_events_from(body)
+    events.each do |event|
+      message = event.message['text'] || event.type
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
+  rescue
+    head :ok
+  end
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = 'cb9fb5cdb7c1c8dca8ab81eb26542515'
+      config.channel_token = 'DSA7s5gRjVPttdQKJz34BcLqwPeBh+Jt5UZrAe1VG/QDOj1bBlDgUbf6yd+kdr7jfgqHyAnKFKLGlDTPl88gYzTmpwSVCzhnH733mrzAyCUsF+rYjXsOChvbXc/AkxqrljEtNWMDO8NlkFgSMFo0IwdB04t89/1O/w1cDnyilFU='
+    end
+  end
+end
